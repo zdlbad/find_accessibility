2cfb32c07db5c6ce5b87d95cdbde5c763516deb9
zdlbad <zdlbad@gmail.com>
2020-12-04 15:27:17 +1100

    change port

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const http = require('http');
-const https = require('https');
 const fs = require('fs');
 
 dotenv.config({ path: './config.env' });
@@ -19,23 +17,9 @@ mongoose
     console.log(`Connected to DB: ${con.connections[0].name} successfully.`);
   });
 
-const httpPort = 3000;
-const httpsPort = 4000;
 const app = require('./app');
 
 
-const privateKey = fs.readFileSync(__dirname + '/selfsigned.key');;
-const certificate = fs.readFileSync(__dirname + '/selfsigned.crt');;
-const credentials = {key: privateKey, cet:certificate};
-
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
-
-
-httpServer.listen(httpPort, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${httpPort} for http...`);
-});
-
-httpsServer.listen(httpsPort, () => {
-  console.log(`Listening on port ${httpsPort} for https...`);
 });
